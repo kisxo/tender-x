f56c1f9da984c15a1dbe0b74ce975970f6a37c4a
uday <udaysubba2004@gmail.com>
2025-04-17 22:33:57 +0530

    Fix: Correct case in routes file path for consistency

--- public/index.php
+++ public/index.php
@@ -6,6 +6,6 @@ require_once '../core/router.php';
 $router = new Router();
 
 // Include the routes from the routes.php file
-require_once '../app/routes/routes.php';  // This will register all routes
+require_once '../App/Routes/routes.php';  // This will register all routes
 
 $router->dispatch($_SERVER['REQUEST_URI']);
