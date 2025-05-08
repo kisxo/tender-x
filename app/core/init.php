0c8becb0307539b3d475a6cef53109ad85858300
uday <udaysubba2004@gmail.com>
2025-05-08 22:04:15 +0530

    Add core application structure with routing and controller loading; remove legacy router

---
+++ app/core/init.php
@@ -0,0 +1,10 @@
+<?php
+
+require "config.php";
+require "functions.php";
+require "Database.php";
+require "Model.php";
+require "Controller.php";
+
+//load the app class after loading the all core
+require "App.php";
--- public/index.php
+++ public/index.php
@@ -1,33 +1,7 @@
 <?php
 
-function show($stuff)
-{
-    echo "<pre>";
-    print_r($stuff);
-    echo "</pre>";
-}
+session_start();
 
+require "../app/core/init.php";
 
-function splitURL()
-{
-    $URL = $_GET['url'] ?? 'home';
-    $URL = explode('/', $URL);
-    return $URL;
-}
-
-function loadController()
-{
-    $URL = splitURL();
-
-    $filename = "../app/controllers/".ucfirst($URL[0]).".php";
-    if (file_exists($filename)) 
-    {
-        require $filename;
-    } else {
-
-        $filename = "../app/controllers/_404.php";
-        require $filename;
-    }
-}
-
-loadController();
+$app = new App();
