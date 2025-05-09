a0f2b6a2fffe5750701a0b6e14cb3c35028e0bbb
uday <udaysubba2004@gmail.com>
2025-05-09 13:14:58 +0530

    update: add querying  the first result

--- app/core/Model.php
+++ app/core/Model.php
@@ -35,7 +35,35 @@ class Model
 
     public function first($data, $data_not = [])
     {
+        $keys = array_keys($data);
+        $keys_not = array_keys($data_not);
 
+        //dynamically select from a table
+        $query = "SELECT * FROM $this->table where ";
+
+        foreach ($keys as $key)
+        {
+            $query .= $key . " = :" . $key . " && ";
+        }
+
+        foreach ($keys_not as $key)
+        {
+            $query .= $key . " != :" . $key . " && ";
+        }
+
+        $query = trim($query, " && ");
+        $query .= " LIMIT $this->limit OFFSET $this->offset";
+
+        // echo $query;
+        $query_data = array_merge($data, $data_not);
+        $result =  $this -> query($query, $query_data);
+
+        if ($result)
+        {
+            return $result[0];
+        }
+
+        return false;
     }
 
     public function insert($data)
