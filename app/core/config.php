b9fc7281d05cdb815c93401fd6020be436d7de40
uday <udaysubba2004@gmail.com>
2025-05-09 23:39:14 +0530

    update: add debug flag in config and toggle error log

--- app/core/config.php
+++ app/core/config.php
@@ -1,21 +1,27 @@
 <?php
 
 // development mode config
-if ($_SERVER['SERVER_NAME'] == 'localhost') {
+if ($_SERVER["SERVER_NAME"] == "localhost") {
     /** database config */
-    define('DBNAME', 'tenderx');
-    define('DBHOST', 'localhost');
-    define('DBUSER', 'root');
-    define('DBPASS', '');
-    define('DBDRIVER', 'mysql');
+    define("DBNAME", "tenderx");
+    define("DBHOST", "localhost");
+    define("DBUSER", "root");
+    define("DBPASS", "");
+    define("DBDRIVER", "mysql");
 
 // production mode config
 } else {
 
     /** database config */
-    define('DBNAME', 'tenderx');
-    define('DBHOST', 'localhost');
-    define('DBUSER', 'root');
-    define('DBPASS', '');
-    define('DBDRIVER', 'mysql');
+    define("DBNAME", "tenderx");
+    define("DBHOST", "localhost");
+    define("DBUSER", "root");
+    define("DBPASS", "");
+    define("DBDRIVER", "mysql");
 }
+
+define("APP_NAME", "Tenderx");
+define("APP_DESC", "Best tender bidding website");
+
+/** [ true=Development / false=Production ] */
+define("DEBUG", true);
--- public/index.php
+++ public/index.php
@@ -1,10 +1,17 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 session_start();
 
 require "../app/core/init.php";
+
+if(DEBUG)
+{
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+} else
+{
+    ini_set('display_errors', 0);
+}
 
 $app = new App();
 $app->loadController();
