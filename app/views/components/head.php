f3a46846559a8110366537e26f5a8826d1b28690
uday <udaysubba2004@gmail.com>
2025-05-10 19:09:42 +0530

    feat: add head component with meta tags and stylesheet link

---
+++ app/views/components/head.php
@@ -0,0 +1,4 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title> <?= $page_title ?> </title>
+<link rel="stylesheet" href="/assets/css/style.css">  <!-- You can add your CSS link here -->
