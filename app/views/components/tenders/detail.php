eb16c98bf05b6849f4a542fefb81bc4e9c0755e1
uday <udaysubba2004@gmail.com>
2025-05-16 02:46:00 +0530

    fix: correct indentation for bidding buttons in tender detail view

--- app/views/components/tenders/detail.php
+++ app/views/components/tenders/detail.php
@@ -27,12 +27,12 @@
         </a>
     <?php endif; ?>
 
-        <a href="/" class="block mt-10">
-            <?php if ($deadline_over): ?>
-                <button disabled type="submit" class="text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-sm text-sm w-full py-2.5 text-center ">Bidding Closed</button>
-            <?php else: ?>
-                <button type="submit" class="text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-sm text-sm w-full py-2.5 text-center ">Place a bid</button>
-            <?php endif; ?>
-        </a>
+    <a href="/" class="block mt-10">
+        <?php if ($deadline_over): ?>
+            <button disabled type="submit" class="text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-sm text-sm w-full py-2.5 text-center ">Bidding Closed</button>
+        <?php else: ?>
+            <button type="submit" class="text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-sm text-sm w-full py-2.5 text-center ">Place a bid</button>
+        <?php endif; ?>
+   </a>
 
 </div>
